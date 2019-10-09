17871268966e275593a32bd8a53393ba33afabb8
chospace7@gmail.com <chospace7@gmail.com>
2019-10-09 16:55:08 +0700

    day3

---
+++ Dicoding/js/angkot/penumpang/angkot.js
@@ -0,0 +1,40 @@
+var angkot = [];
+var tambahPenumpang = function( namaPenumpang, angkot ) {
+
+    if ( angkot.length == 0 ){
+        angkot.push(namaPenumpang);
+        return angkot;
+    } else {
+        for ( var i = 0; i < angkot.length; i++ ){
+            if(angkot[i] == undefined ) {
+                angkot[i] = namaPenumpang;
+                return angkot;
+            }
+            else if (angkot[i] == namaPenumpang) {
+                console.log(namaPenumpang + ' sudah ada di dalam angkot');
+                return angkot;
+            }
+            else if ( i == angkot.length -1 ) {
+                angkot.push(namaPenumpang);
+                return angkot;
+            }
+        }
+    }
+}
+
+var hapusPenumpang = function(namaPenumpang, angkot) {
+    if (angkot.length == 0) {
+        console.log('Angkot masih kosong');
+        return hapusPenumpang;
+    } else {
+        for(var i = 0; i < angkot.length; i++){
+            if(angkot[i] == namaPenumpang){
+                angkot[i] = undefined;
+                return angkot;
+            } else if (i == angkot.length - 1){
+                console.log(namaPenumpang + ' tidak ada di dalam angkot');
+                return angkot;
+            }
+        }
+    }
+}
--- Dicoding/js/angkot/penumpang/penumpang.js
+++ Dicoding/js/angkot/penumpang/penumpang.js
@@ -34,3 +34,20 @@ var tambahPenumpang = function(namaPenumpang, penumpang) {
         }
     }
 } 
+
+var hapusPenumpang = function(namaPenumpang, penumpang) {
+    if (penumpang.length == 0) {
+        console.log('penumpang masih kosong');
+    } else {
+        for(var i = 0; i < penumpang.length; i++){
+            if(penumpang[i] == namaPenumpang){
+                penumpang[i] = undefined;
+                return penumpang;
+            } else if (i == penumpang.length - 1){
+                console.log(namaPenumpang + ' tidak ada di dalam angkot');
+                return penumpang;
+            }
+        }
+    }
+    return hapusPenumpang;
+}
