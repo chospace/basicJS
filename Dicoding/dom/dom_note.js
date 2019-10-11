ba8827989eb404bc4077b1ff5722883bb8a5daad
chospace7@gmail.com <chospace7@gmail.com>
2019-10-12 00:17:07 +0700

    dom selection

---
+++ Dicoding/dom/dom_note.js
@@ -0,0 +1,8 @@
+// DOM SELECTION METHOD
+
+// METHOD                       --> Hasil
+getElementById()                --> element
+getElementByTagName()           --> HTMLCollection
+getELementByClassName()         --> HTMLCollection
+querySelector()                 --> element
+querySelectorAll()              --> nodeList
